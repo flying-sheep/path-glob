7ef47c54518711ec1254c161dd2b793267af7091
Philipp A. <flying-sheep@web.de>
2016-05-12 17:07:10 +0100

    Update documentation

--- implementors/path_glob/trait.Glob.js
+++ implementors/path_glob/trait.Glob.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['glob'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='glob/struct.Paths.html' title='glob::Paths'>Paths</a>",];
+implementors["path_glob"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
